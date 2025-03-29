19a6e89c32de0a350686d5e288446ebf08390a02
MukulTiwari02 <mukultiwari2020@gmail.com>
2025-03-29 22:55:40 +0530

    Changed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,17 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    headers: {
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin",
-    },
-    proxy: {
-      "/cdn": {
-        target: "https://unpkg.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cdn/, ""),
-      },
-    },
-  },
 });
